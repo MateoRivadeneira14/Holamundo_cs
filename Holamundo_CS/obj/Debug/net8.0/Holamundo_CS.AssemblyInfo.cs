a9ed8efbe70f935017c132882f0148b108e849b3
MateoRivadeneira14 <mateoriva98@gmail.com>
2024-11-11 08:15:41 -0500

    feat: :art: Add image
    Build a new folder named images and put the image in

--- Holamundo_CS/obj/Debug/net8.0/Holamundo_CS.AssemblyInfo.cs
+++ Holamundo_CS/obj/Debug/net8.0/Holamundo_CS.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Holamundo_CS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93abf0a8a599a6130112c1939174173368516173")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5072d2a23e8deacee389047e939022ce645eb59")]
 [assembly: System.Reflection.AssemblyProductAttribute("Holamundo_CS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Holamundo_CS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
